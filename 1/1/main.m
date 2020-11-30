ba046c9f23c7a1155f5511a50005cf18c9bdfb0b
林珈卉 <linjiahui@linjiahuideMacBook-Pro-2.local>
2020-11-30 14:26:38 +0800

    First Commit

---
+++ 1/1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  1
+//
+//  Created by 林珈卉 on 2020/11/30.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
